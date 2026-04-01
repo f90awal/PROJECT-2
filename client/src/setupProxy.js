4512201f05898f24cf62b852de713069bd44ae70
Awal <155694249+f90awal@users.noreply.github.com>
2026-04-01 12:53:33 +0000

    internal_error

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const target = process.env.GATEWAY_BASE_URL || 'https://gateway-production-7565.up.railway.app';
+
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target,
       changeOrigin: true,
     })
   );
